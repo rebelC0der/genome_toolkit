064f66325f2928b52c7e87f8571ee1822d997169
rebelCoder <jurisl@pm.me>
2022-10-15 18:24:22 +0100

    Genome Toolkit. Part 2: in search of patterns

--- genome_toolkit.py
+++ genome_toolkit.py
@@ -1,3 +1,11 @@
 class genomeToolkit:
     def __init__(self):
-        print("Genome Toolkit initiated")
+        print("[Genome Toolkit Initiated]\n")
+
+    def count_kmer(self, sequence, kmer):
+        """Counts repeating k-mers in a sequence. Includes overlapping k-mers"""
+        kmer_count = 0
+        for position in range(len(sequence) - 1):
+            if sequence[position:position + len(kmer)] == kmer:
+                kmer_count += 1
+        return kmer_count
